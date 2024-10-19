fea44cc19d51b5cf89d28e0f1d9d03e363082019
gladsonchala <gladsonchala@gmail.com>
2024-10-19 18:26:57 +0300

    feat: authentication addition and few fix

--- apispark/routers/router.py
+++ apispark/routers/router.py
@@ -5,17 +5,17 @@ class Router:
     def __init__(self):
         self.router = APIRouter()
 
-    def register_routes(self, module_globals):
+    def register_routes(self, module_globals, auth=None):
         for name, func in module_globals.items():
             if inspect.isfunction(func):
                 if name.startswith('get_'):
-                    self.router.get(f"/{name[4:]}")(func)
+                    self.router.get(f"/{name[4:]}")(auth.api_key_required(func) if auth else func)
                 elif name.startswith('post_'):
-                    self.router.post(f"/{name[5:]}")(func)
+                    self.router.post(f"/{name[5:]}")(auth.api_key_required(func) if auth else func)
                 elif name.startswith('put_'):
-                    self.router.put(f"/{name[4:]}")(func)
+                    self.router.put(f"/{name[4:]}")(auth.api_key_required(func) if auth else func)
                 elif name.startswith('delete_'):
-                    self.router.delete(f"/{name[7:]}")(func)
+                    self.router.delete(f"/{name[7:]}")(auth.api_key_required(func) if auth else func)
 
     def include_in_app(self, app):
         app.include_router(self.router)
